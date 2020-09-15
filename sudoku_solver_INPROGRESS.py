4e3a38ca48dfca1ec4aab734e31f643ef2c3950e
hongytan <40774356+hongytan@users.noreply.github.com>
2020-09-15 15:38:39 -0700

    Finished sudoku using recursion

--- sudoku_solver_INPROGRESS.py
+++ sudoku_solver_INPROGRESS.py
@@ -1,164 +1,57 @@
-# Sudoku solver
-# Can only handle really easy ones currently
-# where some empty boxes only have one possible value
+# Sudoku solver using recursion
 
+import pygame
 import numpy as np
 
 grid = [
-    [5,0,0,0,8,6,0,0,1],
-    [0,0,2,7,0,1,6,0,0],
-    [0,7,1,0,0,0,2,5,0],
-    [9,1,0,0,2,0,0,7,0],
-    [3,0,0,1,4,5,0,0,6],
-    [0,6,0,0,9,0,0,2,4],
-    [0,5,3,0,0,0,4,6,0],
-    [0,0,8,9,0,3,5,0,0],
-    [2,0,0,5,1,0,0,0,7]
+    [0,1,0,0,5,0,0,9,0],
+    [0,0,6,9,2,0,0,0,5],
+    [0,5,0,0,0,4,0,6,0],
+    [0,8,5,0,0,0,0,0,0],
+    [0,0,9,7,0,6,8,0,0],
+    [0,0,0,0,0,0,2,7,0],
+    [0,9,0,5,0,0,0,8,0],
+    [5,0,0,0,8,1,9,0,0],
+    [0,3,0,0,9,0,0,2,0],
     ]
-
-# Creates a 3x3 box around grid[x][y]
-def box_possible(x,y):
-    if x % 3 == 0:
-        box = []
-        box.append(grid[x][y])
-        box.append(grid[x+1][y])
-        box.append(grid[x+2][y])
-        if y % 3 == 0:
-            box.append(grid[x][y+1])
-            box.append(grid[x+1][y+1])
-            box.append(grid[x+2][y+1])
-            box.append(grid[x][y+2])
-            box.append(grid[x+1][y+2])
-            box.append(grid[x+2][y+2])
-        elif y % 3 == 1:
-            box.append(grid[x][y-1])
-            box.append(grid[x+1][y-1])
-            box.append(grid[x+2][y-1])
-            box.append(grid[x][y+1])
-            box.append(grid[x+1][y+1])
-            box.append(grid[x+2][y+1])
-        elif y % 3 == 2:
-            box.append(grid[x][y-1])
-            box.append(grid[x+1][y-1])
-            box.append(grid[x+2][y-1])
-            box.append(grid[x][y-2])
-            box.append(grid[x+1][y-2])
-            box.append(grid[x+2][y-2])
-    elif x % 3 == 1:
-        box = []
-        box.append(grid[x][y])
-        box.append(grid[x-1][y])
-        box.append(grid[x+1][y])
-        if y % 3 == 0:
-            box.append(grid[x][y+1])
-            box.append(grid[x-1][y+1])
-            box.append(grid[x+1][y+1])
-            box.append(grid[x][y+2])
-            box.append(grid[x-1][y+2])
-            box.append(grid[x+1][y+2])
-        elif y % 3 == 1:
-            box.append(grid[x][y-1])
-            box.append(grid[x-1][y-1])
-            box.append(grid[x+1][y-1])
-            box.append(grid[x][y+1])
-            box.append(grid[x-1][y+1])
-            box.append(grid[x+1][y+1])
-        elif y % 3 == 2:
-            box.append(grid[x][y-1])
-            box.append(grid[x-1][y-1])
-            box.append(grid[x+1][y-1])
-            box.append(grid[x][y-2])
-            box.append(grid[x-1][y-2])
-            box.append(grid[x+1][y-2])
-    elif x % 3 == 2:
-        box = []
-        box.append(grid[x][y])
-        box.append(grid[x-1][y])
-        box.append(grid[x-2][y])
-        if y % 3 == 0:
-            box.append(grid[x][y+1])
-            box.append(grid[x-1][y+1])
-            box.append(grid[x-2][y+1])
-            box.append(grid[x][y+2])
-            box.append(grid[x-1][y+2])
-            box.append(grid[x-2][y+2])
-        elif y % 3 == 1:
-            box.append(grid[x][y-1])
-            box.append(grid[x-1][y-1])
-            box.append(grid[x-2][y-1])
-            box.append(grid[x][y+1])
-            box.append(grid[x-1][y+1])
-            box.append(grid[x-2][y+1])
-        elif y % 3 == 2:
-            box.append(grid[x][y-1])
-            box.append(grid[x-1][y-1])
-            box.append(grid[x-2][y-1])
-            box.append(grid[x][y-2])
-            box.append(grid[x-1][y-2])
-            box.append(grid[x-2][y-2])
-    return box
 
 def possible(x,y,n):
     """ Finds the possible numbers for each box (if any) """
-
-    # Makes sure box is empty
-    if grid[x][y] == 0:
-        
-        # Checks column and row for same numbers
-        for i in range(9):
-            if grid[x][i] == n or grid[i][y] == n:
-                return False
-
-        # Checks 3x3 box for same numbers
-        box = box_possible(x,y)
-        if n in box:
+    
+    # Checks column and row for same numbers
+    for i in range(9):
+        if grid[x][i] == n or grid[i][y] == n:
             return False
-        
-    else:
-        return False
+
+    # Checks 3x3 box for same numbers
+    x0 = (x) // 3 * 3
+    y0 = (y) // 3 * 3
+
+    for i in range(3):
+        for j in range(3):
+            if grid[x0 + i][y0 + j] == n:
+                return False
+    
+    return True
 
 
-# Main 
-def main():    
+# Solves the sudoku puzzle 
+def solve():    
     """ Prints out all the numbers for empty boxes """
     
     # Adds all possible values for a box to a list
-    # Inputs the possible value if box can only be one value
     for j in range(9):
         for k in range(9):
-            possible_list = []
-            for i in range(1, 10):
-                if possible(j,k,i) != False:
-                     possible_list.append(i)
-            if len(possible_list) == 1:
-                grid[j][k] = possible_list[0]
-
-    # Creates a grid after a solve and inputs all values on new grid as one list
-    grid_after = []
-    for j in range(9):
-        for k in range(9):
-            grid_after.append(grid[j][k])
-
-    # Prints grid in matrix form
+            if grid[j][k] == 0:
+                for i in range(1, 10):
+                    if possible(j,k,i):
+                        grid[j][k] = i
+                        solve()
+                        grid[j][k] = 0
+                        
+                return
+            
     print(np.matrix(grid))
+
+solve()
     
-    # Keep solving until there are no more zeroes
-    if 0 in grid_after:
-        main()
-
-
-main()
-
-
-
-
-
-
-
-
-                
-
-                
-
-                
-
