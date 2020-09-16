7e4f214a46b9fe9c077d3bd5e6afa42c3b2f53d7
hongytan <40774356+hongytan@users.noreply.github.com>
2020-09-15 20:21:03 -0700

    Turtles race now and game stops when there is a winner

--- turtle_race_INPROGRESS.py
+++ turtle_race_INPROGRESS.py
@@ -1,6 +1,7 @@
 # Turtle race
 
 import turtle
+import random
 
 wn = turtle.Screen()
 wn.setup(width=800, height=600)
@@ -13,13 +14,16 @@ pen.penup()
 pen.hideturtle()
 pen.speed(0)
 
+GAME_ON_OFF = True
+
 class Racers():
     def __init__(self, x, y, color):
         self.x = x
         self.y = y
         self.color = color
         self.heading = 90
-        self.dy = 0.05
+        self.dy = random.randrange(5, 20, 1) / 100
+        self.score = 0
 
     def update(self):
         self.y += self.dy
@@ -32,19 +36,61 @@ class Racers():
         pen.goto(self.x, self.y)
         pen.stamp()
 
+    def winner(self):
+        global GAME_ON_OFF
+        if self.y > 285:
+            self.score += 1
+            GAME_ON_OFF = False
+
 racer_1 = Racers(0, 0, "green")
+racer_2 = Racers(100, 0, "white")
+racer_3 = Racers(-100, 0, "red")
+racer_4 = Racers(50, 0, "light blue")
+racer_5 = Racers(-50, 0, "yellow")
 
 racers = []
 racers.append(racer_1)
+racers.append(racer_2)
+racers.append(racer_3)
+racers.append(racer_4)
+racers.append(racer_5)
+
+for racer in racers:
+    print(racer.score)
 
 # Main loop
-while True:
+while GAME_ON_OFF:
     pen.clear()
 
     for racer in racers:
         racer.update()
-
-    for racer in racers:
         racer.render(pen)
+        racer.winner()
 
     wn.update()
+
+
+for racer in racers:
+    print(racer.score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
