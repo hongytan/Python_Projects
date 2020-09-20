e7253c9c34eac71230f4566413a855bf270d1ced
hongytan <40774356+hongytan@users.noreply.github.com>
2020-09-20 14:43:01 -0700

    reads test.txt file

--- login_system_INPROGRESS.py
+++ login_system_INPROGRESS.py
@@ -1,3 +1,9 @@
 # Login system
 
-f = open('words.rtf', 'r')
+with open("test.txt", 'r') as rf:
+    characters = 10
+    files_read = rf.read(characters)
+
+    while len(files_read) > 0:
+        print(files_read, end='$')
+        files_read = rf.read(characters)
