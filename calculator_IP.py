bd16767949b05bc148f49a4edaee0ba472752ee9
hongytan <40774356+hongytan@users.noreply.github.com>
2020-09-21 23:19:26 -0700

    made text bigger

--- calculator_IP.py
+++ calculator_IP.py
@@ -71,26 +71,26 @@ def clear():
     screen.delete(0, len(screen.get()))
 
 # Sets up the GUI for buttons
-button_7 = tk.Button(root, text='7', command=lambda: insert(7)).place(rely=0.2, relwidth=0.25, relheight=0.2)
-button_8 = tk.Button(root, text='8', command=lambda: insert(8)).place(relx=0.25, rely=0.2, relwidth=0.25, relheight=0.2)
-button_9 = tk.Button(root, text='9', command=lambda: insert(9)).place(relx=0.5, rely=0.2, relwidth=0.25, relheight=0.2)
+button_7 = tk.Button(root, text='7', font=('Courier',20), command=lambda: insert(7)).place(rely=0.2, relwidth=0.25, relheight=0.2)
+button_8 = tk.Button(root, text='8', font=('Courier',20), command=lambda: insert(8)).place(relx=0.25, rely=0.2, relwidth=0.25, relheight=0.2)
+button_9 = tk.Button(root, text='9', font=('Courier',20), command=lambda: insert(9)).place(relx=0.5, rely=0.2, relwidth=0.25, relheight=0.2)
 
-button_4 = tk.Button(root, text='4', command=lambda: insert(4)).place(rely=0.4, relwidth=0.25, relheight=0.2)
-button_5 = tk.Button(root, text='5', command=lambda: insert(5)).place(relx=0.25, rely=0.4, relwidth=0.25, relheight=0.2)
-button_6 = tk.Button(root, text='6', command=lambda: insert(6)).place(relx=0.5, rely=0.4, relwidth=0.25, relheight=0.2)
+button_4 = tk.Button(root, text='4', font=('Courier',20), command=lambda: insert(4)).place(rely=0.4, relwidth=0.25, relheight=0.2)
+button_5 = tk.Button(root, text='5', font=('Courier',20), command=lambda: insert(5)).place(relx=0.25, rely=0.4, relwidth=0.25, relheight=0.2)
+button_6 = tk.Button(root, text='6', font=('Courier',20), command=lambda: insert(6)).place(relx=0.5, rely=0.4, relwidth=0.25, relheight=0.2)
 
-button_1 = tk.Button(root, text='1', command=lambda: insert(1)).place(rely=0.6, relwidth=0.25, relheight=0.2)
-button_2 = tk.Button(root, text='2', command=lambda: insert(2)).place(relx=0.25, rely=0.6, relwidth=0.25, relheight=0.2)
-button_3 = tk.Button(root, text='3', command=lambda: insert(3)).place(relx=0.5, rely=0.6, relwidth=0.25, relheight=0.2)
+button_1 = tk.Button(root, text='1', font=('Courier',20), command=lambda: insert(1)).place(rely=0.6, relwidth=0.25, relheight=0.2)
+button_2 = tk.Button(root, text='2', font=('Courier',20), command=lambda: insert(2)).place(relx=0.25, rely=0.6, relwidth=0.25, relheight=0.2)
+button_3 = tk.Button(root, text='3', font=('Courier',20), command=lambda: insert(3)).place(relx=0.5, rely=0.6, relwidth=0.25, relheight=0.2)
 
-button_0 = tk.Button(root, text='0', command=lambda: insert(0)).place(relx=0.25, rely=0.8, relwidth=0.25, relheight=0.2)
-button_clear = tk.Button(root, text='clear', command=clear).place(rely=0.8,relwidth=0.25,relheight=0.2)
-button_equal = tk.Button(root, text='=', command=equal).place(relx=0.5, rely=0.8, relwidth=0.25, relheight=0.2)
+button_0 = tk.Button(root, text='0', font=('Courier',20), command=lambda: insert(0)).place(relx=0.25, rely=0.8, relwidth=0.25, relheight=0.2)
+button_clear = tk.Button(root, text='clear', font=('Courier',15), command=clear).place(rely=0.8,relwidth=0.25,relheight=0.2)
+button_equal = tk.Button(root, text='=', font=('Courier',20), command=equal).place(relx=0.5, rely=0.8, relwidth=0.25, relheight=0.2)
  
-button_add = tk.Button(root, text='+', fg='red', command=add).place(relx=0.75, rely=0.2, relwidth=0.25, relheight=0.2)
-button_sub = tk.Button(root, text='-', fg='red', command=sub).place(relx=0.75, rely=0.4, relwidth=0.25, relheight=0.2)
-button_div = tk.Button(root, text='/', fg='red', command=div).place(relx=0.75, rely=0.6, relwidth=0.25, relheight=0.2)
-button_mul = tk.Button(root, text='*', fg='red', command=mul).place(relx=0.75, rely=0.8, relwidth=0.25, relheight=0.2)
+button_add = tk.Button(root, text='+', fg='red', font=('Courier',20), command=add).place(relx=0.75, rely=0.2, relwidth=0.25, relheight=0.2)
+button_sub = tk.Button(root, text='-', fg='red', font=('Courier',20), command=sub).place(relx=0.75, rely=0.4, relwidth=0.25, relheight=0.2)
+button_div = tk.Button(root, text='/', fg='red', font=('Courier',20), command=div).place(relx=0.75, rely=0.6, relwidth=0.25, relheight=0.2)
+button_mul = tk.Button(root, text='*', fg='red', font=('Courier',20), command=mul).place(relx=0.75, rely=0.8, relwidth=0.25, relheight=0.2)
 
 
 
